3bd6fea819f97dedaab185f87fd1fab08771a56f
Sébastien <sebastien@shyrogan.fr>
2024-11-15 15:29:44 +0000

    feat: `RDFMatcher` to match properly with enum constant

---
+++ src/main/java/qengine/storage/RDFMatcher.java
@@ -0,0 +1,116 @@
+package qengine.storage;
+
+import fr.boreal.model.logicalElements.api.Substitution;
+import fr.boreal.model.logicalElements.api.Variable;
+import fr.boreal.model.logicalElements.impl.SubstitutionImpl;
+import qengine.model.RDFAtom;
+
+import java.util.*;
+import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
+public enum RDFMatcher {
+
+    VAR_LIT_LIT(
+            atom -> atom.getTripleSubject().isVariable() && !atom.getTriplePredicate().isVariable()
+                    && !atom.getTripleObject().isVariable(),
+            (store, atom) -> {
+                var results = store.pos.getOrDefault(store.index(atom.getTriplePredicate()), new HashMap<>())
+                        .getOrDefault(store.index(atom.getTripleObject()), new HashSet<>())
+                        .stream()
+                        .map(i -> (Substitution) new SubstitutionImpl(Map.of((Variable) atom.getTripleSubject(), store.dict.get(i))))
+                        .collect(Collectors.toSet());
+                return results.iterator();
+            }),
+
+    VAR_VAR_LIT(
+            atom -> atom.getTripleSubject().isVariable() && atom.getTriplePredicate().isVariable()
+                    && !atom.getTripleObject().isVariable(),
+            (store, atom) -> {
+                var results = store.ops.getOrDefault(store.index(atom.getTripleObject()), new HashMap<>())
+                        .entrySet().stream()
+                        .flatMap(entry -> entry.getValue().stream()
+                                .map(i -> (Substitution) new SubstitutionImpl(Map.of(
+                                        (Variable) atom.getTripleSubject(), store.dict.get(entry.getKey()),
+                                        (Variable) atom.getTriplePredicate(), store.dict.get(i)))))
+                        .collect(Collectors.toSet());
+                return results.iterator();
+            }),
+
+    VAR_VAR_VAR(
+            atom -> atom.getTripleSubject().isVariable() && atom.getTriplePredicate().isVariable()
+                    && atom.getTripleObject().isVariable(),
+            (store, atom) -> {
+                var results = store.spo.entrySet().stream()
+                        .flatMap(subjectEntry -> subjectEntry.getValue().entrySet().stream()
+                                .flatMap(predicateEntry -> predicateEntry.getValue().stream()
+                                        .map(objectId -> (Substitution) new SubstitutionImpl(Map.of(
+                                                (Variable) atom.getTripleSubject(), store.dict.get(subjectEntry.getKey()),
+                                                (Variable) atom.getTriplePredicate(), store.dict.get(predicateEntry.getKey()),
+                                                (Variable) atom.getTripleObject(), store.dict.get(objectId))))))
+                        .collect(Collectors.toSet());
+                return results.iterator();
+            }),
+
+    LIT_VAR_VAR(
+            atom -> !atom.getTripleSubject().isVariable() && atom.getTriplePredicate().isVariable()
+                    && atom.getTripleObject().isVariable(),
+            (store, atom) -> {
+                var results = store.spo.getOrDefault(store.index(atom.getTripleSubject()), new HashMap<>())
+                        .entrySet().stream()
+                        .flatMap(predicateEntry -> predicateEntry.getValue().stream()
+                                .map(objectId -> (Substitution) new SubstitutionImpl(Map.of(
+                                        (Variable) atom.getTriplePredicate(), store.dict.get(predicateEntry.getKey()),
+                                        (Variable) atom.getTripleObject(), store.dict.get(objectId)))))
+                        .collect(Collectors.toSet());
+                return results.iterator();
+            }),
+
+    LIT_LIT_VAR(
+            atom -> !atom.getTripleSubject().isVariable() && !atom.getTriplePredicate().isVariable()
+                    && atom.getTripleObject().isVariable(),
+            (store, atom) -> {
+                var results = store.spo.getOrDefault(store.index(atom.getTripleSubject()), new HashMap<>())
+                        .getOrDefault(store.index(atom.getTriplePredicate()), new HashSet<>())
+                        .stream()
+                        .map(objectId -> (Substitution) new SubstitutionImpl(Map.of(
+                                (Variable) atom.getTripleObject(), store.dict.get(objectId))))
+                        .collect(Collectors.toSet());
+                return results.iterator();
+            }),
+
+    LIT_VAR_LIT(
+            atom -> !atom.getTripleSubject().isVariable() && atom.getTriplePredicate().isVariable()
+                    && !atom.getTripleObject().isVariable(),
+            (store, atom) -> {
+                var results = store.spo.getOrDefault(store.index(atom.getTripleSubject()), new HashMap<>())
+                        .entrySet().stream()
+                        .filter(entry -> entry.getValue().contains(store.index(atom.getTripleObject())))
+                        .map(entry -> (Substitution) new SubstitutionImpl(Map.of(
+                                (Variable) atom.getTriplePredicate(), store.dict.get(entry.getKey()))))
+                        .collect(Collectors.toSet());
+                return results.iterator();
+            }),
+    LIT_LIT_LIT(
+            atom -> !atom.getTripleSubject().isVariable() && !atom.getTriplePredicate().isVariable()
+                    && !atom.getTripleObject().isVariable(),
+            (store, atom) -> Collections.singleton((Substitution)new SubstitutionImpl(Collections.emptyMap())).iterator());
+
+    private final Predicate<RDFAtom> predicate;
+    private final BiFunction<RDFHexaStore, RDFAtom, Iterator<Substitution>> extractor;
+
+    RDFMatcher(Predicate<RDFAtom> predicate, BiFunction<RDFHexaStore, RDFAtom, Iterator<Substitution>> extractor) {
+        this.predicate = predicate;
+        this.extractor = extractor;
+    }
+
+    public boolean matches(RDFAtom atom) {
+        return predicate.test(atom);
+    }
+
+    public Iterator<Substitution> substitution(RDFHexaStore hexaStore, RDFAtom atom) {
+        return extractor.apply(hexaStore, atom);
+    }
+}
+
